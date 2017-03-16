61163a843556b1c929c524a4e53e274d265d7a42
Robert Zetzsche <zetzsche@movisens.com>
2017-03-16 12:19:45 +0100

    deleted not needed method

--- sensorlibrary/src/main/java/de/kit/sensorlibrary/sensor/accelerometersensor/AccelerometerSensorChangedListener.java
+++ sensorlibrary/src/main/java/de/kit/sensorlibrary/sensor/accelerometersensor/AccelerometerSensorChangedListener.java
@@ -6,6 +6,4 @@ import de.kit.sensorlibrary.sensor.ValueChangedListener;
  * Created by Robert on 03.02.2015.
  */
 public interface AccelerometerSensorChangedListener extends ValueChangedListener<AccelerometerChangedEvent> {
-    @Override
-    void onValueChanged(AccelerometerChangedEvent e);
 }
